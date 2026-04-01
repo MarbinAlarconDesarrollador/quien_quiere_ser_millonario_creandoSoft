1509c466f9e390df56600d6d489b2debffa79684
Marbin Alarcon <marbinalarcon9@gmail.com>
2026-04-01 10:04:56 -0500

    V2

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const CACHE_NAME = 'millonario-v1';
-const assets = ['./', './index.html', './img/pwa-icon-192.png', './img/pwa-icon-512.png'];
+const CACHE_NAME = 'millonario-v2';
+const assets = ['./', 
+        './index.html', 
+         './manifest.json',
+        './img/pwa-icon-192.png', 
+        './img/pwa-icon-512.png'];
 
 self.addEventListener('install', e => {
     e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
@@ -8,3 +12,4 @@ self.addEventListener('install', e => {
 self.addEventListener('fetch', e => {
     e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
+
